0bdbb64e4738831e83e462f4bd10856a9096ee90
etkenned <36723249+etkenned@users.noreply.github.com>
2019-09-24 18:32:48 -0700

    Add files via upload

---
+++ Assets/Character/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public CharacterController2D controller; // references the character controller script from GitHub
+
+    public float runSpeed = 40f; //sets the players movement speed
+    float horizontalMove = 0f; //stores the direction the player is facing
+    bool jump = false; // is the player jumping curently?
+    // Update is called once per frame
+    void Update()
+    {
+      horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; //get input for movement as a number value ### Ex: [left arrow] = -1 and [right arrow] = 1
+      if(Input.GetButtonDown("Jump"))
+      {
+        jump = true;
+      }
+    }
+
+    void FixedUpdate()// move the player character
+    { //fixedDeltaTime ensures that the player speed is always the same across all platforms and systems
+      controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump); //gets input for (moveing left and right, crouching, jumping)
+      jump = false; //prevents the player from jumping forever
+
+    }
+}
